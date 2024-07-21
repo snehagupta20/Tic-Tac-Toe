139b5dc0aa30cfd4e91d939f99f3227778163b68
sneha gupta <gsneha20012004@gmail.com>
2024-07-21 14:21:26 +0530

    log  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
+import Log from './components/Log';
 
 const App = () => {
 
   const [activePlayer, setActivePlayer] = useState('O');
+  // const [gameTurns, setGameTurns] = useState([]);
 
   function handleSelectSquare(){
       setActivePlayer((currActivePlayer) => currActivePlayer === 'O' ? 'X' : 'O');
+      // setGameTurns();
   }
 
   return (
@@ -19,7 +22,7 @@ const App = () => {
         </ol>
         <GameBoard onSelectSq={handleSelectSquare} activeSymbol={activePlayer}></GameBoard>
       </div>
-      log
+      <Log></Log>
     </main>
   )
 }
---
+++ src/components/Log.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+ 
+//output info about the different turns of the players (to analyse the game after)
+const Log = () => {
+  return (
+    <ol id='log'>
+      
+    </ol>
+  )
+}
+
+export default Log
